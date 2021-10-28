f65d14bcb85becd570fe3a38a3c82b44656100bf
Sia <s44bhati@uwaterloo.ca>
2021-10-28 01:26:15 -0400

    Everything integrated onto one page

---
+++ authorization_code/public/script.js
@@ -0,0 +1,11 @@
+console.log("i hate u js");
+
+
+function check(){
+    var hours = document.getElementById("hours").value;
+    var minutes = document.getElementById("minutes").value;
+    var ampm = document.getElementById("ampm").value;
+    console.log(hours);
+    console.log(minutes);
+    console.log(ampm);
+};
